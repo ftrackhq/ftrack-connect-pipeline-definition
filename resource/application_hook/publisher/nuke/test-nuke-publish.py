e1135d8e3d64cba3d761824ab290786ab93bd62e
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2019-04-17 12:44:57 +0200

    model nuke publish

---
+++ resource/application_hook/publisher/nuke/test-nuke-publish.py
@@ -0,0 +1,70 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2019 ftrack
+
+import ftrack_api
+from ftrack_connect_pipeline import constants
+import json
+
+
+def register_publisher(event):
+    # return json so we can validate it
+    return json.dumps(
+        {
+            "name": "Test Publisher",
+            "package": "testPkg",
+            "host":"nuke",
+            "ui":"qt",
+            "context":[
+                {
+                    "name": "context selector",
+                    "plugin": "context.publish",
+                    "widget": "context.publish"
+                }
+            ],
+            "components":{
+                "main":{
+                    "collect":[
+                        {
+                            "name": "collect scene",
+                            "plugin": "nukescene",
+                        }
+                    ],
+                    "validate":[
+                        {
+                            "name": "validate selection",
+                            "plugin":"nonempty"
+                        }
+                    ],
+                    "output":[
+                        {
+                            "name": "write nuke script",
+                            "plugin": "nukescript"
+                        }
+                    ]
+                }
+            },
+            "publish":[
+                {
+                    "name": "to ftrack server",
+                    "plugin":"result",
+                    "visible":False
+                }
+            ]
+        }
+    )
+
+
+def register(api_object, **kw):
+    '''Register plugin to api_object.'''
+
+    # Validate that api_object is an instance of ftrack_api.Session. If not,
+    # assume that _register is being called from an incompatible API
+    # and return without doing anything.
+    if not isinstance(api_object, ftrack_api.Session):
+        # Exit to avoid registering this plugin again.
+        return
+
+    api_object.event_hub.subscribe(
+        'topic={} and data.pipeline.type=publisher'.format(constants.PIPELINE_REGISTER_TOPIC),
+        register_publisher
+    )
