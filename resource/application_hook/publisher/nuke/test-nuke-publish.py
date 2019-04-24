05ebcfdda2a0913cd778d7b7ac1ce1359ac0469f
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2019-04-24 10:28:52 +0200

    add reviewable

--- resource/application_hook/publisher/nuke/test-nuke-publish.py
+++ resource/application_hook/publisher/nuke/test-nuke-publish.py
@@ -93,6 +93,32 @@ def register_publisher(event):
                             "plugin": "thumbnail"
                         }
                     ]
+                },
+                "reviewable": {
+                    "collect": [
+                        {
+                            "name": "collect write node.",
+                            "plugin": "write_node_result",
+                            "widget": "write_node_result"
+                        }
+                    ],
+                    "validate": [
+                        {
+                            "name": "validate selection",
+                            "plugin": "nonempty"
+                        },
+                        {
+                            "name": "validate node type",
+                            "plugin": "node_type",
+                            "options": {"node_type": "Write"}
+                        }
+                    ],
+                    "output": [
+                        {
+                            "name": "write reviewable",
+                            "plugin": "reviewable"
+                        }
+                    ]
                 }
             },
             "publish":[
