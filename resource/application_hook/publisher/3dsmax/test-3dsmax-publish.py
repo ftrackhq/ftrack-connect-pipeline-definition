63195fbd834001bf81a97d72f6835a28386a1e1a
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2019-06-20 17:11:15 +0200

    rename extract to output

--- resource/application_hook/publisher/3dsmax/test-3dsmax-publish.py
+++ resource/application_hook/publisher/3dsmax/test-3dsmax-publish.py
@@ -39,7 +39,7 @@ def register_publisher(event):
                     "output": [
                         {
                             "name": "3dsmaxalembic",
-                            "plugin": "ExtractMaxAlembicPlugin"
+                            "plugin": "OutputMaxAlembicPlugin"
                         }
                     ]
                 },
