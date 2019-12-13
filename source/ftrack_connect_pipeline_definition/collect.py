b24d98c7e0d5818fba9f29c1607236c28284cebb
Lluis Casals <lluis.casals@ftrack.com>
2019-12-13 14:29:08 +0100

    fixed small syntax errors

--- source/ftrack_connect_pipeline_definition/collect.py
+++ source/ftrack_connect_pipeline_definition/collect.py
@@ -10,12 +10,14 @@ def collect_json(sourcePath, event):
     jsonFiles_l = glob.glob(sourcePath + '/*.json')
     loadedJsons_l = []
     for file_s in jsonFiles_l:
+        datastore = None
         with open(file_s, 'r') as f:
             try:
                 datastore = json.load(f)
             except Exception as e:
                 logger.warning("{0} could not be registered, reason: {1}".format(file_s, e))
 
-        loadedJsons_l.append(datastore)
+        if datastore != None:
+            loadedJsons_l.append(datastore)
 
     return json.dumps(loadedJsons_l)
