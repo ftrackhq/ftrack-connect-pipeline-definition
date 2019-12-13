9cb98053de668c148a0b17cac02b7a16b6574ec0
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2019-12-13 14:48:41 +0100

    update schema registration

---
+++ resource/application_hook/schema/register.py
@@ -0,0 +1,24 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2019 ftrack
+
+import ftrack_api
+from ftrack_connect_pipeline import constants
+import os
+from ftrack_connect_pipeline_definition import collect
+from functools import partial
+
+
+def register(api_object, **kw):
+    '''Register plugin to api_object.'''
+
+    # Validate that api_object is an instance of ftrack_api.Session. If not,
+    # assume that _register is being called from an incompatible API
+    # and return without doing anything.
+    if not isinstance(api_object, ftrack_api.Session):
+        # Exit to avoid registering this plugin again.
+        return
+
+    api_object.event_hub.subscribe(
+        'topic={} and data.pipeline.type="schema"'.format(constants.PIPELINE_REGISTER_TOPIC),
+        partial(collect.collect_json, os.path.dirname(__file__))
+    )
