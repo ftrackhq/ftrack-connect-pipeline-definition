3ee17c9e27dba406e1f4aebadbedbf2e6306390a
Lluis Casals <lluis.casals@ftrack.com>
2020-09-17 16:26:59 +0200

    create write node on fly, new plugins added

---
+++ resource/plugins/nuke/publisher/collectors/selection.py
@@ -0,0 +1,29 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2014-2020 ftrack
+
+import ftrack_api
+
+import nuke
+
+from ftrack_connect_pipeline_nuke import plugin
+
+
+class CollectSelectedNodesNukePlugin(plugin.PublisherCollectorNukePlugin):
+    plugin_name = 'selection'
+
+    def run(self, context=None, data=None, options=None):
+
+        selected_nodes = nuke.selectedNodes()
+        selected_node_names = []
+        for node in selected_nodes:
+            selected_node_names.append(node.knob('name').value())
+        return selected_node_names
+
+
+def register(api_object, **kw):
+    if not isinstance(api_object, ftrack_api.Session):
+        # Exit to avoid registering this plugin again.
+        return
+    plugin = CollectSelectedNodesNukePlugin(api_object)
+    plugin.register()
+
--- resource/plugins/nuke/publisher/output/thumbnail.py
+++ resource/plugins/nuke/publisher/output/thumbnail.py
@@ -7,6 +7,7 @@ import tempfile
 import nuke
 
 from ftrack_connect_pipeline_nuke import plugin
+from ftrack_connect_pipeline_nuke.utils import custom_commands as nuke_utils
 
 
 class OutputThumbnailPlugin(plugin.PublisherOutputNukePlugin):
@@ -14,7 +15,12 @@ class OutputThumbnailPlugin(plugin.PublisherOutputNukePlugin):
 
     def run(self, context=None, data=None, options=None):
         node_name = data[0]
-        write_node = nuke.toNode(node_name)
+        input_node = nuke.toNode(node_name)
+        selected_nodes = nuke.selectedNodes()
+        nuke_utils.cleanSelection()
+
+        write_node = nuke.createNode('Write')
+        write_node.setInput(0, input_node)
 
         # create reformat ftrack_object
         reformat_node = nuke.nodes.Reformat()
@@ -42,6 +48,13 @@ class OutputThumbnailPlugin(plugin.PublisherOutputNukePlugin):
         # delete thumbnail network after render
         nuke.delete(reformat_node)
         nuke.delete(new_write_node)
+
+        # delete temporal write node
+        nuke.delete(write_node)
+        # restore selection
+        nuke_utils.cleanSelection()
+        for node in selected_nodes:
+            node['selected'].setValue(True)
 
         component_name = options['component_name']
         return {component_name: file_name}
