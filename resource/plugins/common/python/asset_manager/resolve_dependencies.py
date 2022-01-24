c0f7727504fb3392091968554f9d884d34bcb778
henrik_norin <henrik.norin@ftrack.com>
2022-01-24 15:18:04 +0100

    Added all default task type resolvers

--- resource/plugins/common/python/asset_manager/resolve_dependencies.py
+++ resource/plugins/common/python/asset_manager/resolve_dependencies.py
@@ -28,25 +28,38 @@ class AssetDependencyResolverPlugin(plugin.AssetManagerResolvePlugin):
         # Any explicit links?
         if entity.get('incoming_links') is not None:
             for entity_link in entity.get('incoming_links'):
-                self.conditional_add_contexts(result, self.get_linked_contexts_recursive(entity_link['from']))
+                self.conditional_add_contexts(
+                    result,
+                    self.get_linked_contexts_recursive(entity_link['from']),
+                )
         # A version?
         if entity.entity_type == 'AssetVersion':
-            self.conditional_add_contexts(result, self.get_linked_contexts_recursive(entity['task']))
+            self.conditional_add_contexts(
+                result, self.get_linked_contexts_recursive(entity['task'])
+            )
         # A task or asset?
         elif 'parent' in entity:
-            self.conditional_add_contexts(result, self.get_linked_contexts_recursive(entity['parent']))
+            self.conditional_add_contexts(
+                result, self.get_linked_contexts_recursive(entity['parent'])
+            )
         # Look at parent project as a lst thing
         elif 'project' in entity:
-            self.conditional_add_contexts(result, self.get_linked_contexts_recursive(entity['project']))
+            self.conditional_add_contexts(
+                result, self.get_linked_contexts_recursive(entity['project'])
+            )
         return result
 
-    def conditional_add_latest_version(self, versions, ctx, asset, asset_type_option):
+    def conditional_add_latest_version(
+        self, versions, ctx, asset, asset_type_option
+    ):
         '''Filter context *ctx* and *asset* against *asset_type_option*, if they pass,
         add latest version to *versions*'''
         # We have a matching asset type, check any filters
         if 'task_names_include' in asset_type_option.get('filters', {}):
             matches_all = True
-            for expression in asset_type_option['filters']['task_names_include']:
+            for expression in asset_type_option['filters'][
+                'task_names_include'
+            ]:
                 if not re.match(expression, ctx['name'].lower()):
                     matches_all = False
                     break
@@ -54,7 +67,9 @@ class AssetDependencyResolverPlugin(plugin.AssetManagerResolvePlugin):
                 return
         if 'task_names_exclude' in asset_type_option.get('filters', {}):
             matches_any = False
-            for expression in asset_type_option['filters']['task_names_exclude']:
+            for expression in asset_type_option['filters'][
+                'task_names_exclude'
+            ]:
                 if re.match(expression, ctx['name'].lower()):
                     matches_any = True
                     break
@@ -62,7 +77,9 @@ class AssetDependencyResolverPlugin(plugin.AssetManagerResolvePlugin):
                 return
         if 'asset_names_include' in asset_type_option.get('filters', {}):
             matches_all = True
-            for expression in asset_type_option['filters']['asset_names_include']:
+            for expression in asset_type_option['filters'][
+                'asset_names_include'
+            ]:
                 if not re.match(expression, asset['name'].lower()):
                     matches_all = False
                     break
@@ -70,7 +87,9 @@ class AssetDependencyResolverPlugin(plugin.AssetManagerResolvePlugin):
                 return
         if 'asset_names_exclude' in asset_type_option.get('filters', {}):
             matches_any = False
-            for expression in asset_type_option['filters']['asset_names_exclude']:
+            for expression in asset_type_option['filters'][
+                'asset_names_exclude'
+            ]:
                 if re.match(expression, asset['name'].lower()):
                     matches_any = True
                     break
@@ -78,7 +97,9 @@ class AssetDependencyResolverPlugin(plugin.AssetManagerResolvePlugin):
                 return
         # All good so far, resolve latest version
         latest_version = None
-        for v in self.session.query('AssetVersion where asset.id={}'.format(asset['id'])):
+        for v in self.session.query(
+            'AssetVersion where asset.id={}'.format(asset['id'])
+        ):
             if v['is_latest_version']:
                 latest_version = v
                 break
@@ -94,14 +115,108 @@ class AssetDependencyResolverPlugin(plugin.AssetManagerResolvePlugin):
                 for asset in ctx['assets']:
                     for asset_type_option in options.get('asset_types', []):
                         if (
-                            asset_type_option['asset_type'] == '*' or
-                            asset['type']['name'].lower() == asset_type_option['asset_type'].lower()
+                            asset_type_option['asset_type'] == '*'
+                            or asset['type']['name'].lower()
+                            == asset_type_option['asset_type'].lower()
                         ):
                             self.conditional_add_latest_version(
-                                versions, ctx, asset, asset_type_option)
+                                versions, ctx, asset, asset_type_option
+                            )
         return versions
 
     # Task type specific resolvers
+
+    def resolve_texture_dependencies(self, contexts, options):
+        '''Find latest texture dependency versions *contexts*, based on task type resolvable
+        asset types supplied *options*'''
+        versions = self.resolve_dependencies(contexts, options)
+        # Perform further texture specific resolves/checks on result here
+        return versions
+
+    def resolve_vehicle_dependencies(self, contexts, options):
+        '''Find latest vehicle dependency versions *contexts*, based on task type resolvable
+        asset types supplied *options*'''
+        versions = self.resolve_dependencies(contexts, options)
+        # Perform further vehicle specific resolves/checks on result here
+        return versions
+
+    def resolve_conform_dependencies(self, contexts, options):
+        '''Find latest conform dependency versions *contexts*, based on task type resolvable
+        asset types supplied *options*'''
+        versions = self.resolve_dependencies(contexts, options)
+        # Perform further conform specific resolves/checks on result here
+        return versions
+
+    def resolve_environment_dependencies(self, contexts, options):
+        '''Find latest environment dependency versions *contexts*, based on task type resolvable
+        asset types supplied *options*'''
+        versions = self.resolve_dependencies(contexts, options)
+        # Perform further environment specific resolves/checks on result here
+        return versions
+
+    def resolve_matte_painting_dependencies(self, contexts, options):
+        '''Find latest matte_painting dependency versions *contexts*, based on task type resolvable
+        asset types supplied *options*'''
+        versions = self.resolve_dependencies(contexts, options)
+        # Perform further matte painting specific resolves/checks on result here
+        return versions
+
+    def resolve_prop_dependencies(self, contexts, options):
+        '''Find latest prop dependency versions *contexts*, based on task type resolvable
+        asset types supplied *options*'''
+        versions = self.resolve_dependencies(contexts, options)
+        # Perform further prop specific resolves/checks on result here
+        return versions
+
+    def resolve_character_dependencies(self, contexts, options):
+        '''Find latest character dependency versions *contexts*, based on task type resolvable
+        asset types supplied *options*'''
+        versions = self.resolve_dependencies(contexts, options)
+        # Perform further character specific resolves/checks on result here
+        return versions
+
+    def resolve_editing_dependencies(self, contexts, options):
+        '''Find latest editing dependency versions *contexts*, based on task type resolvable
+        asset types supplied *options*'''
+        versions = self.resolve_dependencies(contexts, options)
+        # Perform further editing specific resolves/checks on result here
+        return versions
+
+    def resolve_production_dependencies(self, contexts, options):
+        '''Find latest production dependency versions *contexts*, based on task type resolvable
+        asset types supplied *options*'''
+        versions = self.resolve_dependencies(contexts, options)
+        # Perform further production specific resolves/checks on result here
+        return versions
+
+    def resolve_modeling_dependencies(self, contexts, options):
+        '''Find latest modeling dependency versions *contexts*, based on task type resolvable
+        asset types supplied *options*'''
+        versions = self.resolve_dependencies(contexts, options)
+        # Perform further modeling specific resolves/checks on result here
+        return versions
+
+    def resolve_previz_dependencies(self, contexts, options):
+        '''Find latest previz dependency versions *contexts*, based on task type resolvable
+        asset types supplied *options*'''
+        versions = self.resolve_dependencies(contexts, options)
+        # Perform further previz specific resolves/checks on result here
+        return versions
+
+    def resolve_tracking_dependencies(self, contexts, options):
+        '''Find latest tracking dependency versions *contexts*, based on task type resolvable
+        asset types supplied *options*'''
+        versions = self.resolve_dependencies(contexts, options)
+        # Perform further tracking specific resolves/checks on result here
+        return versions
+
+    def resolve_rigging_dependencies(self, contexts, options):
+        '''Find latest rigging dependency versions *contexts*, based on task type resolvable
+        asset types supplied *options*'''
+        versions = self.resolve_dependencies(contexts, options)
+        # Perform further rigging specific resolves/checks on result here
+        return versions
+
     def resolve_animation_dependencies(self, contexts, options):
         '''Find latest animation dependency versions *contexts*, based on task type resolvable
         asset types supplied *options*'''
@@ -109,41 +224,138 @@ class AssetDependencyResolverPlugin(plugin.AssetManagerResolvePlugin):
         # Perform further animation specific resolves/checks on result here
         return versions
 
+    def resolve_fx_dependencies(self, contexts, options):
+        '''Find latest fx dependency versions *contexts*, based on task type resolvable
+        asset types supplied *options*'''
+        versions = self.resolve_dependencies(contexts, options)
+        # Perform further fx specific resolves/checks on result here
+        return versions
+
+    def resolve_lighting_dependencies(self, contexts, options):
+        '''Find latest lighting dependency versions *contexts*, based on task type resolvable
+        asset types supplied *options*'''
+        versions = self.resolve_dependencies(contexts, options)
+        # Perform further lighting specific resolves/checks on result here
+        return versions
+
+    def resolve_rotoscoping_dependencies(self, contexts, options):
+        '''Find latest rotoscoping dependency versions *contexts*, based on task type resolvable
+        asset types supplied *options*'''
+        versions = self.resolve_dependencies(contexts, options)
+        # Perform further rotoscoping specific resolves/checks on result here
+        return versions
+
+    def resolve_compositing_dependencies(self, contexts, options):
+        '''Find latest compositing dependency versions *contexts*, based on task type resolvable
+        asset types supplied *options*'''
+        versions = self.resolve_dependencies(contexts, options)
+        # Perform further compositing specific resolves/checks on result here
+        return versions
+
+    def resolve_deliverable_dependencies(self, contexts, options):
+        '''Find latest deliverable dependency versions *contexts*, based on task type resolvable
+        asset types supplied *options*'''
+        versions = self.resolve_dependencies(contexts, options)
+        # Perform further deliverable specific resolves/checks on result here
+        return versions
+
+    def resolve_layout_dependencies(self, contexts, options):
+        '''Find latest layout dependency versions *contexts*, based on task type resolvable
+        asset types supplied *options*'''
+        versions = self.resolve_dependencies(contexts, options)
+        # Perform further layout specific resolves/checks on result here
+        return versions
+
+    def resolve_rendering_dependencies(self, contexts, options):
+        '''Find latest rendering dependency versions *contexts*, based on task type resolvable
+        asset types supplied *options*'''
+        versions = self.resolve_dependencies(contexts, options)
+        # Perform further rendering specific resolves/checks on result here
+        return versions
+
+    def resolve_concept_art_dependencies(self, contexts, options):
+        '''Find latest concept art dependency versions *contexts*, based on task type resolvable
+        asset types supplied *options*'''
+        versions = self.resolve_dependencies(contexts, options)
+        # Perform further concept art specific resolves/checks on result here
+        return versions
+
     def run(self, context_data=None, data=None, options=None):
         # Load and check supplied context
         context_id = data['context_id']
-        context = self.session.query('Context where id={}'.format(context_id)).one()
+        context = self.session.query(
+            'Context where id={}'.format(context_id)
+        ).one()
         if context.entity_type != 'Task':
-            return (False, {'message': 'Asset resolve can only be performed on tasks!'})
+            return (
+                False,
+                {'message': 'Asset resolve can only be performed on tasks!'},
+            )
 
         # Fetch all linked asset containers (contexts: shots, asset builds, sequences and so on)
         linked_contexts = self.get_linked_contexts_recursive(context)
 
         # Define task type resolver mappings
         TASK_TYPE_RESOLVERS = {
+            'texture': self.resolve_texture_dependencies,
+            'conform': self.resolve_conform_dependencies,
+            'environment': self.resolve_environment_dependencies,
+            'matte painting': self.resolve_matte_painting_dependencies,
+            'prop': self.resolve_prop_dependencies,
+            'character': self.resolve_character_dependencies,
+            'editing': self.resolve_editing_dependencies,
+            'production': self.resolve_production_dependencies,
+            'modeling': self.resolve_modeling_dependencies,
+            'previz': self.resolve_previz_dependencies,
+            'tracking': self.resolve_tracking_dependencies,
+            'rigging': self.resolve_rigging_dependencies,
             'animation': self.resolve_animation_dependencies,
-            # TODO: Will define resolvers for all built in task types here
-            '*':self.resolve_dependencies,
+            'fx': self.resolve_fx_dependencies,
+            'lighting': self.resolve_lighting_dependencies,
+            'rotoscoping': self.resolve_rotoscoping_dependencies,
+            'compositing': self.resolve_compositing_dependencies,
+            'deliverable': self.resolve_deliverable_dependencies,
+            'layout': self.resolve_layout_dependencies,
+            'rendering': self.resolve_rendering_dependencies,
+            'concept art': self.resolve_concept_art_dependencies,
+            '*': self.resolve_dependencies,
         }
 
         # Extract task type resolver options from schema
-        resolver_options = options['task_types'].get(context['type']['name'].lower())
+        resolver_options = options['task_types'].get(
+            context['type']['name'].lower()
+        )
         if resolver_options is None:
             # Options for this resolver not defined, any generic?
             resolver_options = options['task_types'].get('*')
             if resolver_options is None:
-                return ([], {'message': 'No asset types defined to resolve for '
-                    '"{}" task type!'.format(context['type']['name'])})
+                return (
+                    [],
+                    {
+                        'message': 'No asset types defined to resolve for '
+                        '"{}" task type!'.format(context['type']['name'])
+                    },
+                )
 
         resolver_name = context['type']['name'].lower()
         if not resolver_name in TASK_TYPE_RESOLVERS:
             resolver_name = '*'
 
         if resolver_name in TASK_TYPE_RESOLVERS:
-            return {'versions': TASK_TYPE_RESOLVERS[resolver_name.lower()](linked_contexts, resolver_options)}
+            return {
+                'versions': TASK_TYPE_RESOLVERS[resolver_name.lower()](
+                    linked_contexts, resolver_options
+                )
+            }
         else:
-            return (False, {'message': 'Do not know how to resolve task type: '
-                '"{}"'.format(context['type']['name'])})
+            return (
+                False,
+                {
+                    'message': 'Do not know how to resolve task type: '
+                    '"{}"'.format(context['type']['name'])
+                },
+            )
+
 
 def register(api_object, **kw):
     if not isinstance(api_object, ftrack_api.Session):
