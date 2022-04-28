f318e5308e7dbf17a7c0b0ccade9ab18ab57de95
lorenzo angeli <hdd@users.noreply.github.com>
2022-04-28 09:40:44 +0200

    play with code

---
+++ resource/plugins/maya/python/publisher/exporters/turntable.py
@@ -0,0 +1,123 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2014-2020 ftrack
+
+import tempfile
+import glob
+
+import maya.cmds as cmds
+
+from ftrack_connect_pipeline_maya import plugin
+import ftrack_api
+
+
+class OutputMayaReviewablePlugin(plugin.PublisherOutputMayaPlugin):
+    plugin_name = 'turntable'
+
+    def run(self, context_data=None, data=None, options=None):
+
+        collected_objects = []
+        for collector in data:
+            collected_objects.extend(collector['result'])
+
+        camera_name = options.get('camera_name', 'persp')
+        if collected_objects:
+            camera_name = collected_objects[0]
+            selected_object = collected_objects[1]
+
+        res_w = int(cmds.getAttr('defaultResolution.width'))
+        res_h = int(cmds.getAttr('defaultResolution.height'))
+
+        sframe = cmds.playbackOptions(q=True, min=True)
+        eframe = cmds.playbackOptions(q=True, max=True)
+
+        locator_to_delete = self.setup_turntable(selected_object)
+        reviwable_path = self.run_reviewable(camera_name, sframe, eframe, res_w, res_h)
+        cmds.delete(locator_to_delete)
+        return reviwable_path
+
+    def setup_turntable(object, sframe, eframe):
+        bb_vertices = cmds.exactWorldBoundingBox(object)
+        x_loc = (bb_vertices[0]+bb_vertices[3])/2
+        y_loc = bb_vertices[1]
+        z_loc = (bb_vertices[2]+bb_vertices[5])/2
+        object_locator = cmds.spaceLocator(
+            absolute=True, 
+            position=[x_loc, y_loc, z_loc],
+            name="object_locator"
+        )
+        cmds.xform(object_locator, centerPivots = True)
+        cmds.setKeyframe(object_locator, attribute="rotateY", value=0, time=sframe)
+        cmds.setKeyframe(object_locator, attribute="rotateY", value=360, time=int(eframe)+1)
+        cmds.keyTangent(object_locator, attribute="rotateY", index=(0, 1),
+                        inTangentType="linear", outTangentType="linear")
+        cmds.orientConstraint(object_locator, object)
+        return object_locator
+
+
+    def run_reviewable(self, camera_name, sframe, eframe, res_w, res_h):
+
+        current_panel = cmds.getPanel(wf=True)
+        panel_type = cmds.getPanel(to=current_panel)  # scriptedPanel
+        if panel_type != 'modelPanel':
+            visible_panels = cmds.getPanel(vis=True)
+            for _panel in visible_panels:
+                if cmds.getPanel(to=_panel) == 'modelPanel':
+                    current_panel = _panel
+                    break
+                else:
+                    current_panel = None
+
+        previous_camera = 'persp'
+        if current_panel:
+            previous_camera = cmds.modelPanel(
+                current_panel, q=True, camera=True
+            )
+
+        cmds.lookThru(camera_name)
+
+        res_w = int(cmds.getAttr('defaultResolution.width'))
+        res_h = int(cmds.getAttr('defaultResolution.height'))
+
+        start_frame = cmds.playbackOptions(q=True, min=True)
+        end_frame = cmds.playbackOptions(q=True, max=True)
+
+        prev_selection = cmds.ls(sl=True)
+        cmds.select(cl=True)
+
+        filename = tempfile.NamedTemporaryFile().name
+
+        cmds.playblast(
+            format='movie',
+            sequenceTime=0,
+            clearCache=1,
+            viewer=0,
+            offScreen=True,
+            showOrnaments=0,
+            frame=range(int(start_frame), int(end_frame + 1)),
+            filename=filename,
+            fp=4,
+            percent=100,
+            quality=70,
+            w=res_w,
+            h=res_h,
+        )
+
+        if len(prev_selection):
+            cmds.select(prev_selection)
+
+        cmds.lookThru(previous_camera)
+
+        temp_files = glob.glob(filename + '.*')
+        # TODO:
+        # find a better way to find the extension of the playblast file.
+        full_path = temp_files[0]
+
+        return [full_path]
+
+
+def register(api_object, **kw):
+    if not isinstance(api_object, ftrack_api.Session):
+        # Exit to avoid registering this plugin again.
+        return
+    plugin = OutputMayaReviewablePlugin(api_object)
+    plugin.register()
