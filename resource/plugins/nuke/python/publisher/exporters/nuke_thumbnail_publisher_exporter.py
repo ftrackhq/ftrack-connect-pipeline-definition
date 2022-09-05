d0aff953bc728fb7fecd9443fba80f34ad1fa9bc
henrik_norin <henrik.norin@ftrack.com>
2022-09-05 14:22:54 +0200

    Add thumbnail to Nuke SCENE Publisher

--- resource/plugins/nuke/python/publisher/exporters/nuke_thumbnail_publisher_exporter.py
+++ resource/plugins/nuke/python/publisher/exporters/nuke_thumbnail_publisher_exporter.py
@@ -13,8 +13,8 @@ from ftrack_connect_pipeline_nuke.utils import custom_commands as nuke_utils
 class NukeThumbnailPublisherExporterPlugin(plugin.NukePublisherExporterPlugin):
     plugin_name = 'nuke_thumbnail_publisher_exporter'
 
-    def run(self, context_data=None, data=None, options=None):
-
+    def render_thumbnail(self, context_data=None, data=None, options=None):
+        '''Render a thumbnail from the first collected node.'''
         collected_objects = []
         for collector in data:
             collected_objects.extend(collector['result'])
@@ -62,6 +62,74 @@ class NukeThumbnailPublisherExporterPlugin(plugin.NukePublisherExporterPlugin):
             node['selected'].setValue(True)
 
         return [file_name]
+
+    def grab_node_graph_thumbnail(
+        self, context_data=None, data=None, options=None
+    ):
+        '''Grab a screenshot of the node graph and use as thumbnail'''
+
+        from PySide2 import QtGui, QtWidgets
+
+        def findviewer():
+            stack = QtWidgets.QApplication.topLevelWidgets()
+            viewers = []
+            while stack:
+                widget = stack.pop()
+                if widget.windowTitle().startswith('Node'):
+                    viewers.append(widget)
+                stack.extend(c for c in widget.children() if c.isWidgetType())
+            print(viewers)
+            return viewers
+
+        def getRelativeFrameGeometry(widget):
+            fg = widget.frameGeometry()
+            left = top = 0
+            while True:
+                g = widget.geometry()
+                left += g.left()
+                top += g.top()
+                widget = widget.parent()
+                if widget is None:
+                    break
+            self.logger.debug(
+                "Grab node graph thumbnail - relative frame geometry; g.left: {}, g.top: {}".format(
+                    left, top
+                )
+            )
+            return fg.translated(left, top)
+
+        def screenCaptureWidget(widget, filename, fileformat='png'):
+            rfg = getRelativeFrameGeometry(widget)
+            pixmap = QtGui.QPixmap.grabWindow(
+                widget.winId(),
+                rfg.left(),
+                rfg.top(),
+                rfg.width(),
+                rfg.height(),
+            )
+            self.logger.debug(
+                "Grab node graph thumbnail - screen; left: {}, top: {}, width: {}, height: {}".format(
+                    rfg.left(), rfg.top(), rfg.width(), rfg.height()
+                )
+            )
+            pixmap.save(filename, fileformat)
+
+        view = findviewer()[1]
+
+        file_name = tempfile.NamedTemporaryFile(
+            delete=False, suffix='.png'
+        ).name
+
+        screenCaptureWidget(view, file_name)
+
+        return [file_name]
+
+    def run(self, context_data=None, data=None, options=None):
+
+        if context_data['asset_type_name'] == 'scene':
+            return self.grab_node_graph_thumbnail(context_data, data, options)
+        else:
+            return self.render_thumbnail(context_data, data, options)
 
 
 def register(api_object, **kw):
