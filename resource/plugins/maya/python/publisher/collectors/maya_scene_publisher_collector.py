c6f0e1217d73853b52f3eb59939f7402aa3fe252
henrik_norin <henrik.norin@ftrack.com>
2022-09-14 13:41:06 +0200

    Fix Maya scene publish collector export option standalone

--- resource/plugins/maya/python/publisher/collectors/maya_scene_publisher_collector.py
+++ resource/plugins/maya/python/publisher/collectors/maya_scene_publisher_collector.py
@@ -12,7 +12,9 @@ class MayaScenePublisherCollectorPlugin(plugin.MayaPublisherCollectorPlugin):
     plugin_name = 'maya_scene_publisher_collector'
 
     def run(self, context_data=None, data=None, options=None):
-        export_option = options.get("export", 'scene')
+        export_option = options.get("export")
+        if export_option and isinstance(export_option, list):
+            export_option = export_option[0]
         if export_option == 'scene':
             scene_name = cmds.file(q=True, sceneName=True)
             if len(scene_name or '') == 0:
